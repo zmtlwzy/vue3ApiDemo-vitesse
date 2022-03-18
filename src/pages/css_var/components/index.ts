c49b4ae0a14f724411af93d5bca7887c13bc59ff
zmtlwzy <1025302051@qq.com>
2022-03-18 12:23:58 +0800

    fix: remove some page

--- src/pages/css_var/components/index.ts
+++
@@ -1,5 +0,0 @@
-import CssvarBase from './CssvarBase.vue'
-import UseCssModule from './UseCssModule.vue'
-import UseCssVars from './UseCssVars.vue'
-
-export { CssvarBase, UseCssModule, UseCssVars }
--- src/pages/event/components/index.ts
+++
@@ -1,4 +0,0 @@
-import EventBase from './EventBase.vue'
-import EventModifiers from './Modifiers.vue'
-
-export { EventBase, EventModifiers }
